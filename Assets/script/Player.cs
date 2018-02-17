780c3d4d7401eb13e0611f679c9b7e3c650816de
Ramon Honorio <ramon.honorio@miraeducacao.com.br>
2018-02-17 17:06:31 -0200

    ice cube topperson

---
+++ Assets/script/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Speed
+	public float speed;
+
+	// Directions
+	float directionX;
+	float initialPositionX, finalPositionX;
+
+	// Positions
+	public Transform[] positions;
+	int currentPosition = 1;
+
+	void Update () {
+		// Fire1 - Left Ctrl || Mouse1 || Touch
+		if (Input.GetButtonDown ("Fire1")) {
+
+			// Position of first touch
+			initialPositionX = Input.mousePosition.x;
+
+		} else if (Input.GetButtonUp ("Fire1")) {
+			// Last touch position
+			finalPositionX = Input.mousePosition.x;
+
+			// Defines direction
+			directionX = finalPositionX - initialPositionX;
+
+			if (directionX > 0 && currentPosition < 2) {
+				currentPosition++;
+			} else if (directionX < 0 && currentPosition > 0) {
+				currentPosition--;
+			}
+		}
+
+		// Repositioning player
+		transform.position = Vector3.Lerp (transform.position, positions[currentPosition].position, speed * Time.deltaTime);
+
+		print (currentPosition);
+	}
+}
