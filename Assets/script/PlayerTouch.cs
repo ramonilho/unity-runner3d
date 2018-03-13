51b04d509430a4c7cf01ded30bfc765f95ab7abc
Ramon Honorio <ramonaqh@gmail.com>
2018-03-13 19:32:19 -0300

    touchable character event - pumpkin

---
+++ Assets/script/PlayerTouch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTouch : MonoBehaviour {
+
+	public GameObject character;
+	public float speed;
+
+	Vector3 newPosition;
+	Animation ani;
+
+	void Start()
+	{
+		newPosition = transform.position;
+		ani = character.GetComponent <Animation>();
+
+		// Sets initial animation
+		ani.CrossFade ("idle");
+	}
+
+	void OnCollisionEnter(Collision c) {
+		if (c.gameObject.tag == "Item") {
+			Destroy (c.gameObject);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1")) {
+			// Transforms touch on screen in a 3D coord
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit)) {
+				Vector3 lockedPos = new Vector3 (hit.point.x, -0.93f, hit.point.z);
+
+				newPosition = lockedPos;
+			}
+			transform.position = Vector3.MoveTowards (transform.position, newPosition, speed * Time.deltaTime);
+			transform.LookAt (hit.point);
+
+			ani.CrossFade ("run");
+		} else {
+			ani.CrossFade ("idle");
+		}
+
+	}
+
+}
---
+++ Assets/script/PumpkinGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices.ComTypes;
+
+public class PumpkinGenerator : MonoBehaviour {
+
+	public float leftLimit, rightLimit, frontLimit, backLimit;
+	public float frequency;
+	public GameObject pumpkinPrefab;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (Random.Range(0.5f, frequency));
+
+		Vector3 position = new Vector3();
+		position.x = Random.Range (leftLimit, rightLimit);
+		position.y = transform.position.y;
+		position.z = Random.Range (frontLimit, backLimit);
+
+		Instantiate (pumpkinPrefab, position, transform.rotation);
+
+		StartCoroutine (Start ());
+
+	}
+
+}
